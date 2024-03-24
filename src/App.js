0046bee37c5449178c871f3f1f2800b2d6ac6e00
Vivek_Sahani <sram39266@gmail.com>
2024-03-25 01:22:36 +0530

    deleted unused files and url of resume is updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import HomePage from "./Components/Pages/HomePage";
 import SkillsPage from './Components/Pages/SkillsPage';
 import ProjectsPage from './Components/Pages/ProjectsPage';
 import ContactPage from './Components/Pages/ContactPage';
-import ServicesPage from './Components/Pages/ServicesPage';
 import ErrorPage from './Components/Pages/ErrorPage';
-import Resume from "./Components/Pages/Resume";
 
 export default function App() {
   return (
@@ -16,9 +14,7 @@ export default function App() {
         <Route path="/portfolio/" element={<HomePage />} />
         <Route path="/portfolio/skills" element={<SkillsPage />} />
         <Route path="/portfolio/projects" element={<ProjectsPage />} />
-        <Route path="/portfolio/resume" element={<Resume />} />
         <Route path="/portfolio/contact" element={<ContactPage />} />
-        <Route path="/portfolio/services" element={<ServicesPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
