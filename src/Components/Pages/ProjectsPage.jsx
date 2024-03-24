6d4b16d7574d51dd7ed2e5a83c640187b3174b4f
Vivek_Sahani <sram39266@gmail.com>
2024-03-25 01:35:13 +0530

    Removed unnecessary dummy project cards

--- src/Components/Pages/ProjectsPage.jsx
+++ src/Components/Pages/ProjectsPage.jsx
@@ -37,18 +37,6 @@ export default function Projects() {
           <p>axios</p>
           <p>cyclic for deploment</p>
         </ProjectsCard>
-        
-        <ProjectsCard bgColor='#96D4D4' source={require('./../../Images/bg3.png')} >
-          <h1>Project Heading</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus hic aperiam fugit minima, temporibus blanditiis corporis excepturi tempore dolorum id optio, nam odio. Est, animi. Hic voluptatibus corporis rem fugit neque? Consequuntur, impedit consequatur. Ex cum vitae ullam. Consequuntur.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus hic aperiam fugit minima, temporibus blanditiis corporis excepturi tempore dolorum id optio, nam odio. Est, animi. Hic voluptatibus corporis rem fugit neque? Consequuntur, impedit consequatur. Ex cum vitae ullam. Consequuntur.</p>
-        </ProjectsCard>
-
-        <ProjectsCard bgColor='#D9EEE1' source={require('./../../Images/image1.jpg')} >
-          <h1>Project Heading</h1>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus hic aperiam fugit minima, temporibus blanditiis corporis excepturi tempore dolorum id optio, nam odio. Est, animi. Hic voluptatibus corporis rem fugit neque? Consequuntur, impedit consequatur. Ex cum vitae ullam. Consequuntur.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus hic aperiam fugit minima, temporibus blanditiis corporis excepturi tempore dolorum id optio, nam odio. Est, animi. Hic voluptatibus corporis rem fugit neque? Consequuntur, impedit consequatur. Ex cum vitae ullam. Consequuntur.</p>
-        </ProjectsCard>
       </div>
     </Layout>
   );
